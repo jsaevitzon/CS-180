1a2af5556e75c450cd7910b04774162aebf49761
Jaryd Saevitzon <jsaevitzon@g.ucla.edu>
2016-12-26 20:08:30 -0800

    fixed timeout problems

--- Algorithm_Practice/Inversion Count.cpp
+++ Algorithm_Practice/Inversion Count.cpp
@@ -1,27 +1,13 @@
 #include <map>
-#include <set>
 #include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <string>
-#include <bitset>
-#include <cstdio>
-#include <limits>
 #include <vector>
-#include <climits>
-#include <cstring>
-#include <cstdlib>
-#include <fstream>
-#include <numeric>
-#include <sstream>
 #include <iostream>
 #include <algorithm>
 #include <unordered_map>
 
 using namespace std;
+
+
 
 
 
@@ -32,23 +18,24 @@ long long merge_and_count(int *arr, int l_end, int r_end, int size)
     
     int* temp  = new int[size];
     
-    int l_c=0, r_c=0, idx=0;
+    int l_c=0, r_c=l_end+1, idx=0;
     long long count = 0;
     
-    while( idx != size)
+    while( idx < size)
         {
         
-        if( l_c==l_end )
+        if( l_c>l_end )
             {
-            while( r_c != r_end)
+            while( r_c <= r_end)
                 {
                 temp[idx] = arr[r_c];
                 r_c++; idx++;
             }
         }
-         else if( r_c==r_end )
+        
+         else if( r_c>r_end )
             {
-            while( l_c != l_end)
+            while( l_c <= l_end)
                 {
                 temp[idx] = arr[l_c];
                 l_c++; idx++;
@@ -65,7 +52,8 @@ long long merge_and_count(int *arr, int l_end, int r_end, int size)
               temp[idx] = arr[r_c];
               r_c++; idx++;
               
-              count += l_end-l_c;
+             
+              count += (l_end+1)-l_c;
           }
           }
         
@@ -81,7 +69,7 @@ long long merge_and_count(int *arr, int l_end, int r_end, int size)
 
 long long count_inversions(int* arr, int size) {
         
-    if(size==1)
+    if(size<=1)
      {
        return 0;      
      }
@@ -89,18 +77,44 @@ long long count_inversions(int* arr, int size) {
     long long inv_count = 0;
     int mid = size/2;
     
-    cout<<"count_inv";
+
     
     
     inv_count  = count_inversions(arr, mid);
     inv_count += count_inversions(arr+mid, size-mid);
     
     //what do i need. array (comes with starting point), array middle (mid), array end ()
-    inv_count += merge_and_count(arr, mid, size-1, size);
+    inv_count += merge_and_count(arr, mid-1, size-1, size);
     
     
     return inv_count;
     
     
     
+}
+
+
+
+int main(){
+    int t;
+    cin >> t;
+    for(int a0 = 0; a0 < t; a0++){
+        int n;
+        cin >> n;
+        int *arr = new int[n];
+        int num;
+        for(int arr_i = 0; arr_i < n; arr_i++){
+           cin >> num;
+           arr[arr_i] = num;
+
+        
+
+        }
+ 
+        cout << count_inversions(arr, n) << endl;
+        delete[] arr;
+    }
+    
+
+    return 0;
 }
